3c1cb146201367dd434e304763aee892bef758f9
Daniel Tchoń <dantch@onet.eu>
2025-12-03 19:30:29 +0100

    Rewrite the start.bat to closer match the actual use case

--- instamaticServer/start.bat
+++
@@ -1,3 +0,0 @@
-cd c:\instamatic\TEMserver          &:: point to your installation directory
-:: call .\venv\Scripts\activate.bat &:: uncomment and point to venv if using one
-py tem_server.py                    &:: use `py`, `py3`, or `python` as needed
---
+++ start.bat
@@ -0,0 +1,3 @@
+cd c:\instamatic\instamatic-tecnai-server  &:: point to your installation directory
+:: call .\venv\Scripts\activate.bat        &:: uncomment and point to venv if using one
+python tem_server.py                       &:: use `py`, `py3`, or `python` as needed
